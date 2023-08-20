b467fe3eb181caf38b8636d1acd51e29ea673c65
AleksandrUA1 <vse.byde.ua@gmail.com>
2023-08-20 13:15:19 +0300

    dided oll task of lesson 19

--- lesson-19/js/script_19_6.js
+++ lesson-19/js/script_19_6.js
@@ -1,29 +1,42 @@
 window.onload = function(){
+    let parentOfSportsmenslist = document.getElementById('sportsmensList')
+    let parentOfPlayersList = document.getElementById('playersList')
     
-
-    
-    let parentOfSportsmenlist = document.getElementsByClassName('sportsmenList')
-    let parentOfPlayerList = document.getElementsByClassName('playersList')
-    
-    for (const sportsmen of parentOfSportsmenlist[0].children) {
-        sportsmen.onclick = function () {
-            let sportsmenValue = sportsmen.innerText
+    parentOfSportsmenslist.addEventListener('click', function (event) {
+        if (event.target.tagName === 'IMG') {
+            let sportsmenValue = event.target.parentElement.innerText
             let player = document.createElement('li')
-            player.innerText = sportsmenValue
-            parentOfPlayerList[0].append(player)
-            sportsmen.remove()
-        }
-    }
+            let icon = document.createElement('img')
+            
+            icon.setAttribute('src','img/left.png')
+            icon.setAttribute('style','width: 15px; margin-top: 4px;')
+            icon.setAttribute('alt','left arrow;')
+            icon.classList.add('hoverIcon')
 
-    for (const player of parentOfPlayerList[0].children) {
-        player.onclick = function () {
-            let playerValue = player.innerText
-            let sportsmen = document.createElement('li')
-            sportsmen.innerText = playerValue
-            parentOfSportsmenlist[0].append(sportsmen)
-            player.remove()
+            player.innerText = sportsmenValue
+            player.append(icon)
+            parentOfPlayersList.append(player)
+            event.target.parentElement.remove()
         }
-    }
+    })
+    
+    parentOfPlayersList.addEventListener('click', function (event) {
+        if (event.target.tagName === 'IMG') {
+            let playerValue = event.target.parentElement.innerText
+            let sportsmen = document.createElement('li')
+            let icon = document.createElement('img')
+            
+            icon.setAttribute('src','img/right.png')
+            icon.setAttribute('style','width: 15px; margin-top: 4px;')
+            icon.setAttribute('alt','right arrow;')
+            icon.classList.add('hoverIcon')
+            
+            sportsmen.innerText = playerValue
+            sportsmen.append(icon)
+            parentOfSportsmenslist.append(sportsmen)
+            event.target.parentElement.remove()
+        }
+    })
 
 }
 
